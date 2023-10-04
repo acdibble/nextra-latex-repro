9646d1948b3b1ef2c630c011b4980e3d7779973a
Andrew Dibble <andrew@chainflip.io>
2023-10-04 15:18:36 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './components/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './theme.config.tsx',
+  ],
+  theme: {},
+  plugins: [],
+};
